f1cc2ccf76a6a18764c699375a0fa40d39000c5a
sankaraxi <sankarfrompalani@gmail.com>
2024-11-29 18:34:54 +0530

    Refactor styles and components for improved UI consistency; add page transition animations

---
+++ src/components/PageTransition.jsx
@@ -0,0 +1,26 @@
+"use client";
+
+import { AnimatePresence, motion } from "framer-motion";
+import { usePathname } from "next/navigation";
+
+
+const PageTransition = ({children}) => {
+    const pathname = usePathname();
+
+    return (
+        <AnimatePresence>
+            <div key={pathname}>
+                <motion.div 
+                    initial={{ opacity: 1 }}
+                    animate={{ opacity: 0, 
+                        transition: {delay:0.5, duration: 0.4, ease: "easeInOut"}
+                    }}
+                    className="h-screen w-screen fixed bg-primary pointer-events-none"
+                />
+                {children}
+            </div>
+        </AnimatePresence>
+    )
+}
+
+export default PageTransition;
